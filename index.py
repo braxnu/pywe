3df38c120837b7f95ced9d5d1e611e4ca8ba68b4
Karol Baczewski <braxnu@gmail.com>
2014-07-12 16:46:43 +0200

    request inspector

--- index.py
+++ index.py
@@ -7,7 +7,10 @@ PORT = 8000
 
 class MyHandler (SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET (s):
-        print(dir(s))
+        for key in dir(s):
+            if hasattr(s, key):
+                print(key + ': ' + str(getattr(s, key))[:80])
+
         return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(s)
 
 
